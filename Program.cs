2f14aaae3bb942485a388763aec323612eca5e76
valeskaandrade <valeska.andrade@gmail.com>
2020-02-11 14:48:46 -0300

    aula 10 meu primeiro programa mostra mensagem ola mundo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace aula10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // meu primeiro programa em C#
+            Console.WriteLine("Olá Mundo!");
+        }
+    }
+}
